d95064098c8ba4b3c5bb156b8a11aaf32efb45b7
Polina Tselischeva <Polina.Tselischeva@intravision.ru>
2024-01-08 02:01:55 +0300

    creating basic frontEnd app based on Seminar Video

---
+++ transparent-chain-management/src/pages/connect.jsx
@@ -0,0 +1,143 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import Web3modal from 'web3modal';
+import { Button, Center, Text, Textarea } from "@mantine/core"
+
+let web3Modal;
+
+const providerOptions = {
+    walletConnect:{
+        package: WalletConnectProvider,
+        options: {
+            rpc:{ 8995: "https://core.bloxberg.org"}
+        }
+    }
+}
+
+if(typeof window != 'undefined'){
+    web3Modal = new Web3modal({
+        cacheProvider:false,
+        providerOptions,
+    })
+}
+
+export default function Connect(){
+
+    const [hasMetamask, setHasMetamask] = useState(false);
+
+    const [signer, setSigner] = useState(undefined);
+
+    const [isConnected, setIsConnected] = useState(false);
+
+    const [contract, setContract] = useState(undefined);
+
+    const [currentUserAddress, setCurrentUserAddress] = useState('');
+
+    const [dataToChain, setDataToChain] = useState('');
+
+    const [dataFromChain, setDataFromChain] = useState('');
+
+    useEffect(()=>{
+        checkMetamask();
+
+        if(isConnected){
+            initialize()
+        }
+        
+    })
+
+    const checkMetamask = () => {
+        if (typeof window.ethereum !== 'undefined'){
+            setHasMetamask(true)
+        }
+        else{
+            setHasMetamask(false)
+        }
+    }
+
+    async function connect(){
+        if(hasMetamask){
+            const web3ModalProvider = await web3Modal.connect()
+            //const provider = new ethers.providers.Web3Provider(web3ModalProvider)
+            const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+
+            setSigner(provider.getSigner())
+            setIsConnected(true)
+        }
+    }
+
+    async function initialize (){
+
+        const contract = new ethers.Contract(contractAdress, abi, signer);
+        const currentUserAddress = await window.ethereum.request({method: 'eth_accounts'})
+
+        setContract(contract)
+        setCurrentUserAddress(currentUserAddress)
+    }
+
+    async function sendData(){
+
+        if(hasMetamask && isConnected){
+            await contract.setData(dataToChain, currentUserAddress[0])
+        }
+    }
+
+    async function getDataFromChain(){
+        const response = await contract.getDataByAddress(currentUserAdress[0])
+
+        let dataArray = []
+
+        response.forEach((data) => {
+
+            dataArray.push(data[1])
+        })
+
+        setDataFromChain(dataArray)
+    }
+
+    return(
+        <div>
+        <Center>
+            {hasMetamask? (
+                isConnected? (
+                    <Center>
+                        <Textarea 
+                        value={dataToChain}
+                        placeholder="store your data to blockchain"
+                        label = {"Add data"}
+                        onChange={(event) => setDataToChain(event.currentTarget.value)}
+                        />
+
+                        <Button onClick={() => sendData()}> Send data</Button>
+
+                        <Button onClick={() => getDataFromChain()}> Get data</Button>
+
+                        
+                    </Center>
+                ): (<Button onClick={() => connect()}>
+                Connect
+                </Button>)                
+            ): ("Download Metamask!")}
+            {/* Has metamask: {JSON.stringify(hasMetamask)}
+            Has connected: {JSON.stringify(isConnected)} */}
+
+            
+        </Center>
+
+        <Center>
+            <div>
+                            {dataFromChain.map((data, index) => (
+                                <div key={index}>
+                                    <Text>
+                                        {data.data}
+                                    </Text>
+                                </div>
+                            ))}
+                        </div>
+        </Center>
+
+        </div>
+                
+    )
+}
